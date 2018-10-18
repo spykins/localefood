274fa20094b0bd0a7699043f0e2846e1cc29d96a
spykins <waleola.akinsanmi@gmail.com>
2018-10-18 17:25:29 -0400

    removing logging in okhttpclient

--- app/src/main/java/com/spykins/localefood/repository/http/RetrofitProvider.java
+++ app/src/main/java/com/spykins/localefood/repository/http/RetrofitProvider.java
@@ -1,7 +1,5 @@
 package com.spykins.localefood.repository.http;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,16 +8,11 @@ public class RetrofitProvider {
     private Retrofit retrofit = null;
 
     public Retrofit provideRetrofit() {
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(logging);
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://api.foursquare.com")
                     .addConverterFactory(GsonConverterFactory.create())
-                    .client(httpClient.build())
                     .build();
         }
         return retrofit;
