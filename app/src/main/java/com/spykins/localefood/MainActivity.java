bdb8e851d1e70d44ebd1dfd63dcccb8d8c3c1a16
spykins <waleola.akinsanmi@gmail.com>
2018-10-18 12:37:04 -0400

    set up viewmodel for the main activity

--- app/src/main/java/com/spykins/localefood/MainActivity.java
+++ app/src/main/java/com/spykins/localefood/MainActivity.java
@@ -1,13 +1,46 @@
 package com.spykins.localefood;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.spykins.localefood.model.Restaurant;
+import com.spykins.localefood.viewmodel.FoodViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private FoodViewModel foodViewModel;
+    private EditText addressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addressText = findViewById(R.id.address_text);
+
+        foodViewModel = ViewModelProviders.of(this).get(FoodViewModel.class);
+        subscribe();
+    }
+
+    private void subscribe() {
+
+        final Observer<List<Restaurant>> restaurants = new Observer<List<Restaurant>>() {
+            @Override
+            public void onChanged(@Nullable List<Restaurant> restaurants) {
+                //Log.d("RESTAURANR", restaurants.toString());
+            }
+        };
+
+        foodViewModel.getRestaurants().observe(this, restaurants);
+    }
+
+    public void onsubmit(View view) {
+
     }
 }
---
+++ app/src/main/java/com/spykins/localefood/viewmodel/FoodViewModel.java
@@ -0,0 +1,24 @@
+package com.spykins.localefood.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import com.spykins.localefood.model.Restaurant;
+
+import java.util.List;
+
+public class FoodViewModel extends AndroidViewModel {
+    private MutableLiveData<List<Restaurant>> restaurants = new MutableLiveData<>();
+
+    //restaurants.postValue when fetched from api
+
+    public FoodViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public MutableLiveData<List<Restaurant>> getRestaurants() {
+        return restaurants;
+    }
+}
